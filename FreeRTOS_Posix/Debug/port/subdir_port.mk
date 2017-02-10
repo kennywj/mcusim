dd38a49c357422f8768279d08079a4db031502e1
kenny <chenwenruei@gmail.com>
2017-02-10 20:54:23 +0800

    add subdir_port.mk

---
+++ FreeRTOS_Posix/Debug/port/subdir_port.mk
@@ -0,0 +1,28 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../port/freertos/ethernetif.c \
+../port/freertos/sys_arch.c
+
+OBJS += \
+./port/ethernetif.o \
+./port/sys_arch.o
+
+C_DEPS += \
+./port/ethernetif.d \
+./port/sys_arch.d
+
+
+# Each subdirectory must supply rules for building sources it contributes
+port/%.o: ../port/freertos/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -D__GCC_POSIX__=1 -DDEBUG_BUILD=1 -DUSE_STDIO=1 -I../. -I../FreeRTOS_Kernel/include -I../port -I../lwip/src/include \
+	-O0 -g -Wall -c -fmessage-length=0 -pthread -lrt -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
